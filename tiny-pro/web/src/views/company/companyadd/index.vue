6a454b0e2328158be6670bca96725f7f3667005b
seeker <gmx472@qq.com>
2025-03-22 21:27:38 +0800

    add company-view && add AutoComplete demo

---
+++ tiny-pro/web/src/views/company/companyadd/index.vue
@@ -0,0 +1,65 @@
+//FIXME: it's just a demo
+<template>
+  <div class="demo-autocomplete">
+    <tiny-autocomplete v-model="value" :fetch-suggestions="querySearch" placeholder="请输入内容"></tiny-autocomplete>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { TinyAutocomplete } from '@opentiny/vue'
+
+const restaurants = ref([])
+const value = ref('')
+
+onMounted(() => {
+  restaurants.value = loadAll()
+})
+
+function querySearch(queryString, cb) {
+  let results = queryString ? restaurants.value.filter(createFilter(queryString)) : restaurants.value
+  // 调用 callback 返回建议列表的数据
+  cb(results)
+}
+
+function createFilter(queryString) {
+  return (restaurant) => restaurant.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+}
+
+function loadAll() {
+  return [
+    {
+      value: 'GFD 科技 YX 公司',
+      address: '福州'
+    },
+    {
+      value: 'WWWW 科技 YX 公司',
+      address: '深圳福田区'
+    },
+    {
+      value: 'RFV 有限责任公司',
+      address: '中山市'
+    },
+    {
+      value: 'TGBYX 公司',
+      address: '梅州'
+    },
+    {
+      value: 'YHN 科技 YX 公司',
+      address: '韶关'
+    },
+    {
+      value: '康康物业 YX 公司',
+      address: '广州天河区'
+    }
+  ]
+}
+</script>
+
+<style scoped>
+.demo-autocomplete .tiny-autocomplete {
+  width: 270px;
+  left: 10px;
+  top: 10px;
+}
+</style>
---
+++ tiny-pro/web/src/views/company/info/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <!-- TODO:Add something here -->
+    <p>TODO!</p>
+    
+  </div>
+</template>
+
